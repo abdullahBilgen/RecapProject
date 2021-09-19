4265c4a3ccc9c8bc5e7bd471d1afd98b98021630
Abdullah Bilgen <57772947+abdullahBilgen@users.noreply.github.com>
2021-09-20 00:22:19 +0300

    commit

---
+++ reCapProject/reCapProject/src/main/java/com/example/reCapProject/api/controller/BrandsController.java
@@ -0,0 +1,58 @@
+package com.example.reCapProject.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.reCapProject.business.abstracts.BrandService;
+import com.example.reCapProject.core.utilities.result.DataResult;
+import com.example.reCapProject.core.utilities.result.Result;
+import com.example.reCapProject.entities.concretes.Brand;
+import com.example.reCapProject.entities.request.CreateBrandRequest;
+
+@RestController
+@RequestMapping("api/brands")
+public class BrandsController {
+
+	BrandService brandService;
+
+	@Autowired
+	public BrandsController(BrandService brandService) {
+		super();
+		this.brandService = brandService;
+	}
+
+	@GetMapping("/getall")
+	public DataResult<List<Brand>> getAll() {
+		return this.brandService.getAll();
+	}
+
+	@PostMapping("/add")
+	public Result add(@Valid @RequestBody CreateBrandRequest createBrandRequest) {
+		return this.brandService.add(createBrandRequest);
+	}
+
+	@GetMapping("/getbyid")
+	public DataResult<Brand> getById(int brandId) {
+		return this.brandService.getById(brandId);
+	}
+
+	@PostMapping("update")
+	public Result update(Brand brand) {
+		return this.brandService.update(brand);
+	}
+
+	@PutMapping("delete")
+	public Result delete(Brand brand) {
+		return this.brandService.delete(brand);
+	}
+
+}
