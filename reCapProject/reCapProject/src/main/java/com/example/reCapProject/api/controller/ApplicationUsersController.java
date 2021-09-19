8ffc8bdcc9c38832cea3d0aa7a13942ca99726c3
Abdullah Bilgen <57772947+abdullahBilgen@users.noreply.github.com>
2021-09-20 00:17:59 +0300

    commit

--- reCapProject/reCapProject/src/main/java/com/example/reCapProject/api/controller/ApplicationUsersController.java
+++ reCapProject/reCapProject/src/main/java/com/example/reCapProject/api/controller/ApplicationUsersController.java
@@ -1,22 +1,12 @@
 package com.example.reCapProject.api.controller;
 
 import java.util.List;
-
-import javax.validation.Valid;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.reCapProject.business.abstracts.ApplicationUserService;
 import com.example.reCapProject.core.entities.concretes.User;
 import com.example.reCapProject.core.utilities.result.DataResult;
-import com.example.reCapProject.core.utilities.result.Result;
-import com.example.reCapProject.entities.request.CreateApplicationUserRequest;
-
 
 
 @RestController
